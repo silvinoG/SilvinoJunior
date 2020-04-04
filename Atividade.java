f442cf1a7fc6483b5b09438d77bd792ce10bd2ca
silvinoG <63107763+silvinoG@users.noreply.github.com>
2020-04-03 21:48:30 -0300

    Trabalho Algoritmos
    Atividade 2

---
+++ Atividade.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Soma {
+    
+    public static void main(String args[]) {
+    
+        String nome = "X";
+        int valor1;
+        int valor2;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o valor1: ");
+        valor1 = sc.nextInt();
+        
+        System.out.println("Digite o valor2: ");
+        valor2 = sc.nextInt();
+        
+        
+            System.out.println("X = " + (valor1 - valor2));
+            
+            
+    }        
+        
+}
