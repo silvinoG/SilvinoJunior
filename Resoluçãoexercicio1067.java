bcbc982a4ce147f8af10703098a4aaecc6765402
Silvino <63107763+silvinoG@users.noreply.github.com>
2020-10-17 16:31:35 -0300

    Numeros Impares
    Numeros Impares

---
+++ Resoluçãoexercicio1067.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+    
+    Scanner sc = new Scanner (System.in);
+    
+    int x;
+    
+    x = sc.nextInt();
+    
+    for(int i = 0; i < x; i++){
+        i = i + 1;
+    System.out.println(i);  
+    }
+    }
+}
