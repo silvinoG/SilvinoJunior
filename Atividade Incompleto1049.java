a3cec07ee1c139a46777c35c7a6962d4bd6e829c
Silvino <63107763+silvinoG@users.noreply.github.com>
2020-11-08 15:02:15 -0300

    Animais
    Animais

---
+++ Atividade Incompleto1049.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+    
+    Scanner sc = new Scanner(System.in);
+    char nome;
+    char tipo;
+    char classe;
+    
+    nome = sc.next().charAt(0);
+    tipo = sc.next().charAt(0);
+    classe = sc.next().charAt(0);
+    
+        if(nome == 'v')
+        if (tipo == 'a')
+        if(classe == 'c'){
+        System.out.println("aguia");
+    }
+        //if(nome == 'v')
+        //if (tipo == 'a')
+        //if(classe == 'o'){
+        //System.out.println("pomba");
+    //}
+        //if(nome == 'v')
+        //if (tipo == 'm')
+        //if(classe == 'o'){
+        //System.out.println("homem");
+    //}
+        //if(nome == 'v')
+        //if (tipo == 'm')
+        //if(classe == 'h'){
+        //System.out.println("vaca");
+    //}
+        //if(nome == 'i')
+        //if (tipo == 'i')
+        //if(classe == 'h'){
+        //System.out.println("pulga");
+    //}
+        //if(nome == 'i')
+        //if (tipo == 'i')
+        //if(classe == 'h'){
+        //System.out.println("lagarta");
+    //}
+    }
+}
