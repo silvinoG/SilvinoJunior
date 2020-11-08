b881ad701420b2ffaeaf89d107899ad29ef031fb
Silvino <63107763+silvinoG@users.noreply.github.com>
2020-11-07 22:27:29 -0300

    Sequencia IJ 2
    Sequencia IJ 2

---
+++ Resoluçãoexercicio1096.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+        
+    Scanner sc = new Scanner (System.in);
+    
+    int i;
+    int j;
+    
+    for(i=0; i<=9; i++){
+        i = i + 1;
+    for(j=7; j>=5; j--)
+    System.out.println("I="+i+" J="+j);
+    }
+    }
+}
