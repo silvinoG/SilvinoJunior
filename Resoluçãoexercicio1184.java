d7bf9952b2f5f9b873ff26f79f2a90974f3172fc
Silvino <63107763+silvinoG@users.noreply.github.com>
2020-12-07 00:36:19 -0300

    Abaixo da Diagonal Principal
    Abaixo da Diagonal Principal

---
+++ Resoluçãoexercicio1184.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+    
+    Scanner sc = new Scanner (System.in);
+    
+    double s = 0;
+    char l = 'S'+'M';
+    double [][] a = new double [12][12]; //matriz de 12
+    
+    l = sc.next().charAt(0); //entrada do caractere 
+    
+    for(int i=0; i<a.length; i++){    // linha da matriz
+        for(int j= 0; j<a[i].length; j++){  //coluna da matriz
+            a[i][j] = sc.nextDouble();   //entrada dos valores 
+        }
+    }
+    for(int i=0; i<a.length; i++){   //linha da matriz
+        for(int j= 0; j<a[i].length; j++){   //coluna da matriz
+        if(j < i)   //valores da diagonal abaixo
+        s = s + a[i][j];   //soma dos valores
+        }
+    }
+    if(l == 'M')   //se l receber M
+        s /= ((a.length * a.length) - 12) / 2; //multiplica a linha pela coluna
+                                              //subtrai pela tamanho da matriz
+                                             //divide pela soma dos valores
+                                            //da matriz diagonal de cima
+      System.out.printf("%.1f\n",s);           
+    }
+}
