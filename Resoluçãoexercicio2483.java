efd11156dbf555f90960ca1acb06c4b405d257c0
Silvino <63107763+silvinoG@users.noreply.github.com>
2021-02-03 22:33:45 -0300

    Feliz Nataaal!
    Feliz Nataaal!

---
+++ Resoluçãoexercicio2483.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Testes {
+    public static void main(String[] args) {
+     
+    Scanner sc = new Scanner(System.in);
+    
+    int n;
+    String a = "a";
+    
+    n = sc.nextInt();
+     
+    for(int i=1; i<n; i++){
+        a += "a";
+    }
+        System.out.println("Feliz nat"+a+"l!");
+    }
+    }
